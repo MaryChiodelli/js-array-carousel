89b6c16fad05952f5a6188a6fdf9b35f19f27d44
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-26 15:49:30 +0200

    add slider markup from javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-console.log('hello');
+const slides = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg'
+];
+
+const slidesWrapperEl = document.querySelector('.slides-wrapper');
+
+// creare il contenuto dello slider
+for (let i = 0; i < slides.length; i++) {
+    // creare l'elemento li
+    const li = document.createElement('li');
+    li.classList.add('slide');
+
+    // aggiungo la classe active al primo list item
+    if (i === 0) {
+        li.classList.add('active');
+    }
+
+    // crare l'elemento img
+    const img = document.createElement('img');
+    img.src = slides[i];
+    
+    // inserire il contenuto dello slider nella pagina
+    li.append(img);
+    slidesWrapperEl.append(li);
+}
+
+console.log(slides);

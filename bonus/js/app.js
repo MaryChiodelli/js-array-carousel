bde522ad37a896f17fd2bf393443a31b47a5a979
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-26 17:18:52 +0200

    add infinite loop

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -37,23 +37,43 @@ for (let i = 0; i < slides.length; i++) {
 arrowRightEL.addEventListener('click', function () {
     // togliere la classe active dal list item corrente
     const currentSlide = slideElements[startIndex];
-    currentSlide.classList.remove('active');
 
-    // aggiungere la classe active al list item successivo
-    const nextSlide = slideElements[startIndex + 1];
-    nextSlide.classList.add('active');
-    startIndex++;
+    // SE list item corrente è l'ultima
+    if (startIndex === slideElements.length - 1) {
+        currentSlide.classList.remove('active');
+
+        // aggiungere la classe active al primo list item
+        const nextSlide = slideElements[0];
+        nextSlide.classList.add('active');
+        startIndex = 0;
+    } else {
+        currentSlide.classList.remove('active');
+
+        // aggiungere la classe active al list item successivo
+        const nextSlide = slideElements[startIndex + 1];
+        nextSlide.classList.add('active');
+        startIndex++;
+    }
 });
 
 arrowLeftEL.addEventListener('click', function () {
     // togliere la classe active dal list item corrente
     const currentSlide = slideElements[startIndex];
-    currentSlide.classList.remove('active');
 
-    // aggiungere la classe active al list item precedente
-    const prevSlide = slideElements[startIndex - 1];
-    prevSlide.classList.add('active');
-    startIndex--;
+    // SE list item corrente è il primo
+    if (startIndex === 0) {
+        currentSlide.classList.remove('active');
+        
+        // aggiungere la classe active all'ultimo list item
+        const prevSlide = slideElements[slideElements.length - 1];
+        prevSlide.classList.add('active');
+        startIndex = slideElements.length - 1;
+    } else {
+        currentSlide.classList.remove('active');
+    
+        // aggiungere la classe active al list item precedente
+        const prevSlide = slideElements[startIndex - 1];
+        prevSlide.classList.add('active');
+        startIndex--;
+    }
 });
-
-console.log(slideElements[0]);
